ea5e16b7cd5ce9a235f59cd3c7348860aec1a8ea
SADIK HYSENI <122787525+SadikHyseni@users.noreply.github.com>
2024-01-14 21:29:10 +0000

    Update Book.h

--- project_library/Book.h
+++ project_library/Book.h
@@ -3,8 +3,10 @@
 
 #include <string>
 
+//book class
 class Book {
 private:
+  //private data for books details
     int bookID;
     std::string bookName;
     int pageCount;
@@ -14,17 +16,19 @@ private:
     std::string dueDate;
 
 public:
+  //constructors 
     Book();
      Book(int id, const std::string& name, int count, const std::string& firstName, const std::string& lastName, const std::string& type);
     // Getter functions
   
 
+  //functions to retrieve for book details
     std::string getBookID() const; 
     const std::string& getBookName() const;
     int getPageCount() const;
     const std::string& getAuthorFirstName() const;
     const std::string& getAuthorLastName() const;
-    const std::string& getBookType() const;;
+    const std::string& getBookType() const;
     // Function to read books from a CSV line
     static Book readBookFromCSV(const std::string& csvLine);
     std::string setDueDate() const;
